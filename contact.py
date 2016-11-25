be779abcd80c533428f80bc516722fc23652f284
PrzemyslawUrbanczyk <przemyslaw.urbanczyk@performante.com>
2016-11-25 15:08:57 +0100

    Added new contacts test

---
+++ contact.py
@@ -0,0 +1,24 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company, address, home_number,
+                 mobile_number, work_number, fax, first_email, second_email, third_email, birth_year,
+                 anniversary_year, second_address, second_private_number, notes):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home_number = home_number
+        self.mobile_number = mobile_number
+        self.work_number = work_number
+        self.fax = fax
+        self.first_email = first_email
+        self. second_email = second_email
+        self.third_email = third_email
+        self.birth_year = birth_year
+        self.anniversary_year = anniversary_year
+        self.second_address=second_address
+        self.second_private_number = second_private_number
+        self.notes = notes
