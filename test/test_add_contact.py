98dd0d7f22549246254f2b1d82ec6629db0aad27
PrzemyslawUrbanczyk <przemyslaw.urbanczyk@performante.com>
2016-12-06 12:02:25 +0100

    Dodano inteligentną fixturę ze sprawdzaniem ważności

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
     
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(first_name="sdfdsfdsf", middle_name="sdfsdfsdf", last_name="sdfsdfsdfds",
                               nickname="sdfsdfsdfsf", title="sfsdfsdfsfd", company="dsfsdfsfdsdfs",
                               address="gsdgdgsdsdgdg", home_number="23423424234", mobile_number="234234324",
@@ -11,7 +10,6 @@ def test_add_contact(app):
                               second_email="sdedfefe@qwe.com", third_email="ddfgsdsffsd@qwe.com", wwwpage="www.yuwqyqwyuqwuywq.com",
                               birth_year="1988", anniversary_year="2015", second_address="dsfdsfsdfsdfsdfsdfd",
                               second_private_number="fdsdfsdfsdfsd", notes="fdsdfssdfdssfdsf"))
-    app.session.logout()
 
 
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,7 +3,6 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_contact(Contact(first_name="modified", middle_name="modified", last_name="modified",
                                nickname="modified", company="modified", title="modified",
                                address="modified", home_number="modified", mobile_number="modified",
@@ -11,7 +10,6 @@ def test_modify_first_contact(app):
                                second_email="modified", third_email="modified", wwwpage="modified",
                                birth_year="3000", anniversary_year="3000", second_address="modified",
                                second_private_number="modified", notes="modified"))
-    app.session.logout()
 
 
 
